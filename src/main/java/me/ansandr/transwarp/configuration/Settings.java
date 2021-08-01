fec79e8de5567eba711e5dc8991b084cd5701daf
Ansandr <ansandr2005@gmail.com>
2021-08-02 00:48:36 +0300

    fixup! Add system for getting variable from configurations

---
+++ src/main/java/me/ansandr/transwarp/configuration/Settings.java
@@ -0,0 +1,63 @@
+package me.ansandr.transwarp.configuration;
+
+import me.ansandr.transwarp.TransWarp;
+import me.ansandr.transwarp.storage.SQLStorageManager;
+import me.ansandr.transwarp.storage.YamlStorageManager;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.Locale;
+import java.util.logging.Logger;
+
+public class Settings {
+
+    private static final Logger LOGGER = TransWarp.getInstance().getLogger();
+    private final FileConfiguration config;
+    private final TransWarp plugin;
+
+    public static String rawVersion;
+    public static int version;
+
+    private String storageType;
+    private boolean isMenuEnable;
+
+    public Settings(final TransWarp plugin) {
+        this.plugin = plugin;
+        config = plugin.getConfig();
+    }
+
+    public  void reloadConfig() {
+        plugin.reloadConfig();
+
+        rawVersion = config.getDefaults().getString("config_version");
+        version = calculateVersion(rawVersion);
+
+        if (calculateVersion(config.getString("config_version")) < version) {
+            LOGGER.warning("Config is outdated!");//TODO update
+        }
+
+
+
+        storageType = config.getString("storage_type".toLowerCase(Locale.ROOT), "yaml");
+        isMenuEnable = config.getBoolean("menu.enable", false);
+    }
+
+    public String getStorageType() {
+        return storageType;
+    }
+
+    public boolean isMenuEnable() {
+        return isMenuEnable;
+    }
+
+    public static int calculateVersion(String s) {
+        int ver = 0;
+        String[] version = s.split("\\.");
+        try {
+            ver += Integer.parseInt(version[0]) * 1000;
+            ver += Integer.parseInt(version[1]) * 100;
+            ver += Integer.parseInt(version[2]);
+        } catch (NumberFormatException ignored) {}
+
+        return ver;
+    }
+}
